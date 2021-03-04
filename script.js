e815b67743cb047e9aa412d346a7c3703299cf0b
KilliBlack <76109292+KilliBlack@users.noreply.github.com>
2021-03-04 09:20:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+var cookie = document.getElementById('cookie')
+var cookie = 0
+var cpc = 1
+var cenaup = 20
+// add cookie score
+function add() {
+    cookie = cookie +cpc
+    document.getElementById('cookie').textContent = cookie
+}
+//upgrades setup
+function up1() {
+    if(cookie >= 20) {
+        cpc += 1
+        cookie -= cenaup
+        cpc += 1
+        document.getElementById('cookie').textContent = cookie
+    }else {
+        alert('HAHAHA seš chudej')
+    }
+}
